29e017400fdb6836af41eb8ba9a07d0a4b66597a
Vevek Selvam <vevek@veveks-macbook-air.local>
2015-10-14 01:35:57 +0800

    Initial Commit

---
+++ Quotamin/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Quotamin
+//
+//  Created by Vevek Selvam on 14/10/15.
+//  Copyright © 2015 Vevek Selvam. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
