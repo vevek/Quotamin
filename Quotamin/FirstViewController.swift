8b7d14635396958e5d811f8d21338918f6362d8e
Vevek Selvam <vevekselvam.dev@gmail.com>
2016-01-05 03:13:48 +0800

    1

--- Quotamin/FirstViewController.swift
+++ Quotamin/FirstViewController.swift
@@ -10,31 +10,15 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-//        
-//        var dateComp:NSDateComponents = NSDateComponents()
-//        dateComp.year = 2014;
-//        dateComp.month = 06;
-//        dateComp.day = 09;
-//        dateComp.hour = 15;
-//        dateComp.minute = 26;
-//        dateComp.timeZone = NSTimeZone.systemTimeZone()
-//        
-//        var calender:NSCalendar = NSCalendar(calendarIdentifier: NSGregorianCalendar)!
-//        var date:NSDate = calender.dateFromComponents(dateComp)!
-//        
-        
-        var notification:UILocalNotification = UILocalNotification()
-        notification.category = "FIRST_CATEGORY"
-        notification.alertBody = "Hi, I am a notification"
-//        notification.fireDate = date
-        
-        UIApplication.sharedApplication().scheduleLocalNotification(notification)
         
         
     }
+    
     
 
     override func didReceiveMemoryWarning() {
@@ -42,6 +26,7 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
