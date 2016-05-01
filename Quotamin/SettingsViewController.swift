84b39a991a31fed58aa512897b11e49bc710f79f
Vevek Selvam <vevek@MacBook-Pro.local>
2016-05-02 03:10:40 +0800

    Dev window working

--- Quotamin/SettingsViewController.swift
+++ Quotamin/SettingsViewController.swift
@@ -6,4 +6,52 @@
 //  Copyright © 2016 Vevek Selvam. All rights reserved.
 //
 
-import Foundation
+
+import UIKit
+import Firebase
+
+class SettingsViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    
+    @IBOutlet weak var QuoteBodyTextField: UITextField!
+    @IBOutlet weak var QuoteAuthorTextField: UITextField!
+    @IBOutlet weak var SendUpdateQuote: UIButton!
+    var A: String!
+    var B: String!
+    
+    
+    
+    var refQuoteBody2 = Firebase(url: "https://dazzling-inferno-8467.firebaseio.com/Quote/QuoteBody/")
+    var refQuoteAuthor2 = Firebase(url: "https://dazzling-inferno-8467.firebaseio.com/Quote/QuoteAuthor/")
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     
+        
+        
+    }
+
+    
+    // MARK: Memory Warning Received
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func QuoteSendButton(sender: AnyObject) {
+        
+        A = QuoteBodyTextField.text
+        B = QuoteAuthorTextField.text
+        refQuoteBody2.setValue(QuoteBodyTextField?.text)
+        refQuoteAuthor2.setValue(QuoteAuthorTextField?.text)
+    }
+    
+}
+
